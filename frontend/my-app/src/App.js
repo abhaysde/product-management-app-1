20311be2ecbaa29e5ce4c1757fbc370c7b01b9ed
abhaysde <abhaysinghcse00003@gmail.com>
2025-10-06 15:30:41 +0530

    updated code
    update code

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './components/LoginPage/LoginPage';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        {/* Add other routes here */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
